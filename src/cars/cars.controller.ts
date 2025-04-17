95c7d6ec08e1367e6d8e1d022e77a9725a50e802
Mauri <mauri.monzon91@gmail.com>
2025-04-17 10:13:06 -0300

    Interface y UUID

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -20,7 +20,8 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
+  //getCarById(@Param('id', ParseIntPipe) id: number) {
+  getCarById(@Param('id') id: string) {
     return this.carsService.findOneById(id);
   }
 
@@ -30,12 +31,14 @@ export class CarsController {
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+  //updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+  updateCar(@Param('id') id: string, @Body() body: any) {
     return body;
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
+  //deleteCar(@Param('id', ParseIntPipe) id: number) {
+  deleteCar(@Param('id') id: string) {
     return {
       method: 'DELETE',
       id,
