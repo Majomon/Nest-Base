926acac04b67e20e8d8375cf053b9bbe1ca77cf8
Mauri <mauri.monzon91@gmail.com>
2025-04-17 10:23:18 -0300

    Parse UUID

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -4,7 +4,7 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
@@ -20,8 +20,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  //getCarById(@Param('id', ParseIntPipe) id: number) {
-  getCarById(@Param('id') id: string) {
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
     return this.carsService.findOneById(id);
   }
 
@@ -31,13 +30,11 @@ export class CarsController {
   }
 
   @Patch(':id')
-  //updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
   updateCar(@Param('id') id: string, @Body() body: any) {
     return body;
   }
 
   @Delete(':id')
-  //deleteCar(@Param('id', ParseIntPipe) id: number) {
   deleteCar(@Param('id') id: string) {
     return {
       method: 'DELETE',
